665de4d6af227d7eab2c94bf27c5b25acef2302c
Benk274 <106601229+Benk274@users.noreply.github.com>
2023-12-31 13:28:39 +0100

    Textstorage changes and delete option.

--- Handslator/src/app/services/text-storage/text-storage.service.ts
+++ Handslator/src/app/services/text-storage/text-storage.service.ts
@@ -6,7 +6,7 @@
  */
 
 
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
@@ -16,10 +16,10 @@ export class TextStorageService {
   private static instance: TextStorageService;
 
   private static text: string;
-  private static shortTextLength:number = 20;
+  private static shortTextLength: number = 20;
 
   private constructor() {
-    TextStorageService.text="";
+    TextStorageService.text = "";
   }
 
   /**
@@ -27,34 +27,46 @@ export class TextStorageService {
    * returns the full text
    * will be used in the text output page
    */
-  public static getFullText():string{
-    if (!TextStorageService.instance){
+  public static getFullText(): string {
+    if (!TextStorageService.instance || TextStorageService.text == "") {
       return "Nothing here yet";
     }
     return TextStorageService.text;
   }
+
   /**
    * @getShortText
    * returns the shortened version of the text
    * will be used in the text component
    */
-  public static getShortText():string{
-    if (!TextStorageService.instance){
+  public static getShortText(): string {
+    if (!TextStorageService.instance || TextStorageService.text == "") {
       return "Show your hands ;)"
     }
-    if (TextStorageService.text.length >TextStorageService.shortTextLength+3){
-      return ("..."+TextStorageService.text.slice(TextStorageService.text.length-TextStorageService.shortTextLength,TextStorageService.text.length));
+    if (TextStorageService.text.length > TextStorageService.shortTextLength + 3) {
+      return ("..." + TextStorageService.text.slice(TextStorageService.text.length - TextStorageService.shortTextLength, TextStorageService.text.length));
     }
     return TextStorageService.text;
   }
+
   /**
    * @setLastValue
    * adds a new value to the String
    */
-  public static setLastValue(value:string){
-    if (!TextStorageService.instance){
-      TextStorageService.instance=new TextStorageService();
+  public static setLastValue(value: string) {
+    if (!TextStorageService.instance) {
+      TextStorageService.instance = new TextStorageService();
     }
     TextStorageService.text = TextStorageService.text.concat(value)
+  }
+
+  /**
+   * @dropData
+   * deletes previous Data.
+   */
+  public static dropData() {
+    if (TextStorageService.instance) {
+      TextStorageService.text = "";
+    }
   }
 }
