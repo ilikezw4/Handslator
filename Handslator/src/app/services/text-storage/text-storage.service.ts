ec6413bc732d51f3ce272be573bd30d5aa5fb44a
Benk274 <106601229+Benk274@users.noreply.github.com>
2024-01-12 10:19:19 +0100

    textstorage ausgabe

--- Handslator/src/app/services/text-storage/text-storage.service.ts
+++ Handslator/src/app/services/text-storage/text-storage.service.ts
@@ -40,7 +40,7 @@ export class TextStorageService {
    */
   public static getShortText(): string {
     if (!TextStorageService.instance || TextStorageService.text == "") {
-      return "";
+      return "Show your hands ; )";
     }
     if (TextStorageService.text.length > TextStorageService.shortTextLength + 3) {
       return ("..." + TextStorageService.text.slice(TextStorageService.text.length - TextStorageService.shortTextLength, TextStorageService.text.length));
