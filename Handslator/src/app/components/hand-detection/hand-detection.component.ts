91931d62a7526f93c4ae3c71dca84d2d63b3fce5
ilikezw4 <92365142+ilikezw4@users.noreply.github.com>
2024-01-12 10:11:39 +0100

    added writing of coordinates (later letters)

--- Handslator/src/app/components/hand-detection/hand-detection.component.ts
+++ Handslator/src/app/components/hand-detection/hand-detection.component.ts
@@ -52,7 +52,8 @@ export class HandDetectionComponent implements AfterViewInit {
       if (this.handLandmarker) {
         const detections = this.handLandmarker.detectForVideo(this.video, this.lastVideoTime);
         if (detections.landmarks.length > 0) {
-          console.log(this.filterData(detections.landmarks));
+          // console.log(this.filterData(detections.landmarks));
+          TextStorageService.setLastValue(this.filterData(detections.landmarks));
           this.drawConnections(detections.landmarks, HAND_CONNECTIONS, {color: '#00FF00', lineWidth: 5});
         }
         this.lastVideoTime = this.video.currentTime;
@@ -146,7 +147,7 @@ export class HandDetectionComponent implements AfterViewInit {
       filteredList[i] = (parseInt(filteredList[i]) - parseInt(basecoordX)).toString();
       filteredList[i + 1] = (parseInt(filteredList[i + 1]) - parseInt(basecoordY)).toString();
     }
-    return ','.concat(this.normalize(filteredList));
+    return this.normalize(filteredList);
   }
 
   private normalize(list: string[]) {
