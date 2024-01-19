060db9fd74f53e8423a88c3d748bc0a4418c84ea
ilikezw4 <92365142+ilikezw4@users.noreply.github.com>
2024-01-19 10:44:18 +0100

    Added copy functionality to CopyButton in "textpage"

--- Handslator/src/app/pages/textpage/textpage.page.ts
+++ Handslator/src/app/pages/textpage/textpage.page.ts
@@ -1,12 +1,25 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, } from '@angular/core';
+import { Clipboard } from '@capacitor/clipboard';
+import {TextStorageService} from "../../services/text-storage/text-storage.service";
 
 @Component({
   selector: 'app-textpage',
   templateUrl: './textpage.page.html',
   styleUrls: ['./textpage.page.scss'],
 })
-export class TextpagePage {
+export class TextpagePage implements AfterViewInit{
 
+  private button!: HTMLButtonElement;
+  async ngAfterViewInit(): Promise<void> {
+    this.button = document.getElementById("copyButton") as HTMLButtonElement;
+    this.button.addEventListener("click", this.buttonClick);
+  }
   constructor() {
+  }
+
+  private async buttonClick(){
+    await Clipboard.write({
+      string: TextStorageService.getFullText()
+    });
   }
 }
