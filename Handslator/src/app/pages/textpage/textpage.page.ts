c7edc7a49022b35d180dda86f9248072ff0a3428
Benk274 <106601229+Benk274@users.noreply.github.com>
2024-01-25 12:25:05 +0100

    Text to speech funktioniert!! mit js code in ts

--- Handslator/src/app/pages/textpage/textpage.page.ts
+++ Handslator/src/app/pages/textpage/textpage.page.ts
@@ -1,25 +1,45 @@
-import {AfterViewInit, Component, } from '@angular/core';
-import { Clipboard } from '@capacitor/clipboard';
+import {AfterViewInit, Component,} from '@angular/core';
+import {Clipboard} from '@capacitor/clipboard';
 import {TextStorageService} from "../../services/text-storage/text-storage.service";
+
 
 @Component({
   selector: 'app-textpage',
   templateUrl: './textpage.page.html',
   styleUrls: ['./textpage.page.scss'],
 })
-export class TextpagePage implements AfterViewInit{
+export class TextpagePage implements AfterViewInit {
 
-  private button!: HTMLButtonElement;
-  async ngAfterViewInit(): Promise<void> {
-    this.button = document.getElementById("copyButton") as HTMLButtonElement;
-    this.button.addEventListener("click", this.buttonClick);
-  }
+  private copybutton!: HTMLButtonElement;
+  private ttsbutton!: HTMLButtonElement;
+
   constructor() {
   }
 
-  private async buttonClick(){
+  async ngAfterViewInit(): Promise<void> {
+    this.copybutton = document.getElementById("copyButton") as HTMLButtonElement;
+    this.copybutton.addEventListener("click", this.copyButtonClick);
+    this.ttsbutton = document.getElementById("ttsButton") as HTMLButtonElement;
+    this.ttsbutton.addEventListener("click", this.ttsButtonClick);
+
+  }
+
+  private async copyButtonClick() {
     await Clipboard.write({
       string: TextStorageService.getFullText()
     });
   }
+
+  private async ttsButtonClick() {
+    // Create a SpeechSynthesisUtterance
+    const utterance = new SpeechSynthesisUtterance(TextStorageService.getFullText());
+
+    // Select a voice
+    const voices = speechSynthesis.getVoices();
+    utterance.voice = voices[0]; // Choose a specific voice
+
+    // Speak the text
+    speechSynthesis.speak(utterance);
+  }
 }
+
